ddf29c461090abd8bf315c2affb38fe17f980d88
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2024-07-02 20:06:14 +0000

    Init project files

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿var rand = new Random();
+
+do
+{
+    var n = rand.Next(1, 42); 
+    Console.WriteLine($"Randomly generated n: {n}");
+    Console.WriteLine($"Fibonacci number at position {n}: {Fibonacci(n)}");
+} while (true);
+
+static int Fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n; 
+    }
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
